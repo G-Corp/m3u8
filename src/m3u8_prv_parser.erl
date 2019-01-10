b1a220bd23e630e828700de4a7269841a8f92156
Ryan SIU <ryan.siu@tfidm.com>
2019-01-10 15:45:05 +0800

    Ignore all comments line during parsing

--- src/m3u8_prv_parser.erl
+++ src/m3u8_prv_parser.erl
@@ -207,6 +207,9 @@ parse([<<?EXT_X_ALLOW_CACHE, Data/binary>>|Lines],
                              end},
         State);
 
+parse([<<?COMMENT, _/binary>>|Lines], M3U8, State) ->
+  parse(Lines, M3U8, State);
+
 % Segment URI
 parse([URI|Lines],
       #{segments := Segments} = M3U8,
